61c68a463df80ded9c8a4b97949427aac8415a8e
Shiho Nagano <shiho@nagano.com>
2021-10-29 09:02:52 -0700

    fixed code

--- Rock-paper-scissors/src/janken_game/WinOrLose.java
+++ Rock-paper-scissors/src/janken_game/WinOrLose.java
@@ -3,13 +3,11 @@ package janken_game;
 public class WinOrLose {
 	private static int point = 0;
 	public static final void decisionWinOrLose(int computerHand,int playerHand) {
-		int playerHandTemp = playerHand;
-		playerHandTemp++;
 		
 		if (playerHand == computerHand) {
 			Panel.headerLabel.setText("Draw!（" + point + "）");
 		}
-		else if ((playerHand == 3 && computerHand == 1)||(playerHandTemp == computerHand)) {
+		else if ((playerHand == 3 && computerHand == 1)||(playerHand == 1 && computerHand == 2)|| (playerHand == 2 && computerHand == 3)) {
 			point+=1;
 			Panel.headerLabel.setText("You won!（" + point + "）");
 		}			
